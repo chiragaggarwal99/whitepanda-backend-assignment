3401e95980a6007c3bb4a8bf43d4626120914893
Chirag Aggarwal <43964026+chiragaggarwal99@users.noreply.github.com>
2019-10-13 17:14:23 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cars', function(req, res) {
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017';
+
+  MongoClient.connect(url, function(err, client){
+    if(err){
+      console.log("Unable to connect to server",err);
+    }else{
+      console.log("Connection established");
+
+      var db = client.db('cars');
+      var collection = db.collection('cars');
+      collection.find({}).toArray(function(err, result){
+        if(err){
+          res.send(err);
+        }else if(result.length){
+          res.render('carslist',{
+            "cars": result
+          });
+        }else{
+          res.send("No documents found");
+        }
+        client.close();
+      });
+    }
+  });
+});
+
+module.exports = router;
